14df5d2233dd9f05833f416e867c8a3fc99f4625
christi <40750751+itschristi@users.noreply.github.com>
2018-12-23 22:11:27 -0600

    Create 06_Functions.swift

---
+++ 06_Functions.swift
@@ -0,0 +1,52 @@
+
+func rowTheBoat() {
+    print("Row, row, row your boat")
+    print("Gently down the stream")
+}
+
+func merrilyDream() {
+    print("Merrily, merrily, merrily, merrily")
+    print("Life is but a dream")
+}
+
+func breatheBetweenVerses() {
+    print("        ~        ")
+}
+
+func test() {
+    print("If you see a crocodile")
+    print("Don't forget to scream!")
+}
+
+rowTheBoat()
+merrilyDream()
+breatheBetweenVerses()
+rowTheBoat()
+test()
+
+print(" ")
+print(" ")
+
+
+/*: 
+  - callout(Exercise): Write a function for an alternate second verse of the song using the laughing submarine function below.
+*/
+
+func verseOne() {
+    rowTheBoat()
+    merrilyDream()
+}
+
+verseOne()
+
+func laughingSubmarine() {
+    print("Ha! Ha! Fooled you all")
+    print("I’m a submarine")
+}
+
+func verseTwo() {
+    rowTheBoat()
+    laughingSubmarine()
+}
+
+verseTwo()

5857cb27e314e3d788c36a8591adfa406fc3f676
christi <40750751+itschristi@users.noreply.github.com>
2018-12-27 14:01:38 -0600

    Create 06.2_Functions.swift

---
+++ 06.2_Functions.swift
@@ -0,0 +1,94 @@
+// Inside one of the functions, make changes that make the whole text mean something different, or make it funnier.
+
+// Original Song: Electro Light Orchestra - "Mr. Blue Sky"
+
+func mainChorus() {
+    print("ELO please tell me why")
+    print("You had to stay inactive for so long (so long)")
+    print("Why did I re-write this song?")
+}
+
+func secondChorus() {
+    print("Hey there mister blue")
+    print("We're so pleased to be with you")
+    print("Look around see what you do")
+    print("Everybody smiles at you")
+}
+
+func verseOne() {
+    print("Sun is shinin' in the sky")
+    print("There ain't a cloud in sight")
+    print("It's stopped rainin' everybody's in a play")
+    print("And you don't know")
+    print("It's a beautiful new day, hey hey")
+}
+
+func verseTwo() {
+    print("Runnin' down the avenue")
+    print("See how the sun shines brightly in the city")
+    print("On the streets where once was pitty")
+    print("Mister blue sky is loving here today, hey, hey")
+}
+
+func verseThree() {
+    print("Hey you with the pretty face")
+    print("Welcome to the human race")
+    print("A celebration, mister blue sky's up there waitin'")
+    print("And today is the day we've waited for")
+    
+    func verseFour() {
+        print("Mister blue sky, mister blue sky")
+        print("Mister blue sky")
+    }
+    
+    func verseFive() {
+        print("Mister blue, you did it right")
+        print("But soon comes mister night creepin' over")
+        print("Now his hand is on your shoulder")
+        print("Never mind I'll remember you this")
+        print("I'll remember you this way")
+    }
+    
+    func verseSix() {
+        print("Mister blue sky")
+    }
+    func betweenVerses() {
+        print("")
+        print("   ~   ")
+        print("")
+    }
+    
+    verseOne()
+    betweenVerses()
+    
+    verseTwo()
+    betweenVerses()
+    
+    mainChorus()
+    betweenVerses()
+    
+    mainChorus()
+    betweenVerses()
+    
+    verseThree()
+    betweenVerses()
+    
+    mainChorus()
+    betweenVerses()
+    
+    secondChorus()
+    betweenVerses()
+    
+    secondChorus()
+    betweenVerses()
+    
+    verseFour()
+    betweenVerses()
+    
+    mainChorus()
+    betweenVerses()
+    
+    secondChorus()
+    betweenVerses()
+    
+    verseFive()

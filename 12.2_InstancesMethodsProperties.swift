928ab17dd0750e7219204620b603322cf0b2903e
christi <40750751+itschristi@users.noreply.github.com>
2019-01-01 00:30:30 -0600

    Create 12.2_InstancesMethodsProperties.swift

---
+++ 12.2_InstancesMethodsProperties.swift
@@ -0,0 +1,13 @@
+// Exercise: Treehouse Pulley
+
+// Create three constants for items of different weights that you'd like to bring up to your fort: 
+// One less than 100, one between 100 and 1000, and one over 1000.
+
+let bagOfChips = TreehousePulley(weightCapacity: 5)
+let stackOfBooks = TreehousePulley(weightCapacity: 350)
+let table = TreehousePulley(weightCapacity: 1100)
+
+
+// Create a second pulley that has a higher capacity and can hold at least ten times the weight of the ricketyRope.
+var ricketyRope = TreehousePulley(weightCapacity: 200)
+var highCapacityRope = TreehousePulley(weightCapacity: 3000)

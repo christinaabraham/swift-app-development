fe3a3fe712fffbd178bcd8dd8d292225c7997403
christi <40750751+itschristi@users.noreply.github.com>
2019-01-04 04:36:48 -0600

    Update 12.2_InstancesMethodsProperties.swift

--- 12.2_InstancesMethodsProperties.swift
+++ 12.2_InstancesMethodsProperties.swift
@@ -3,11 +3,39 @@
 // Create three constants for items of different weights that you'd like to bring up to your fort: 
 // One less than 100, one between 100 and 1000, and one over 1000.
 
-let bagOfChips = TreehousePulley(weightCapacity: 5)
-let stackOfBooks = TreehousePulley(weightCapacity: 350)
-let table = TreehousePulley(weightCapacity: 1100)
-
-
 // Create a second pulley that has a higher capacity and can hold at least ten times the weight of the ricketyRope.
+
+
+let bagOfChips = 5
+let stackOfBooks = 350
+let wardrobe = 1000
+
 var ricketyRope = TreehousePulley(weightCapacity: 200)
-var highCapacityRope = TreehousePulley(weightCapacity: 3000)
+var strongerRope = TreehousePulley(weightCapacity: 4000)
+
+// Test bag of chips
+if ricketyRope.canHandleAdditionalLoad(bagOfChips) {
+    ricketyRope.addLoadToBasket(loadWeight: bagOfChips)
+} else if strongerRope.canHandleAdditionalLoad(bagOfChips) {
+    strongerRope.addLoadToBasket(loadWeight: bagOfChips)
+} else {
+    print("Weight capacity is too high for the strong rope.")
+}
+
+// Test stack of books
+if ricketyRope.canHandleAdditionalLoad(stackOfBooks) {
+    ricketyRope.addLoadToBasket(loadWeight: stackOfBooks)
+} else if strongerRope.canHandleAdditionalLoad(stackOfBooks) {
+    strongerRope.addLoadToBasket(loadWeight: stackOfBooks)
+} else {
+    print("Weight capacity is too high for the strong rope.")
+}
+
+// Test wardrobe
+if ricketyRope.canHandleAdditionalLoad(loadWeight: wardrobe) {
+    ricketyRope.addLoadToBasket(loadWeight: wardrobe)
+} else if strongerRope.canHandleAdditionalLoad(wardrobe){
+    strongerRope.addLoadToBasket(loadWeight: wardrobe)
+} else {
+    print("Weight capacity is too high for the strong rope.")
+}

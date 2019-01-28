0d172937bf2496cd512cef4e567d3efb3aad05ee
christi <40750751+itschristi@users.noreply.github.com>
2019-01-28 15:42:40 -0600

    Update 13_QuestionBot.swift

--- 13_QuestionBot/QuestionBot/QuestionBot/13_QuestionBot.swift
+++ 13_QuestionBot/QuestionBot/QuestionBot/13_QuestionBot.swift
@@ -9,6 +9,10 @@ func responseTo(question: String) -> String {
         return "Why, hello there"
     } else if question.hasPrefix("where") {
         return "To the North!"
+    } else if question.hasPrefix("when") {
+        return "How about tomorrow afternoon?"
+    } else if question.hasPrefix("goodbye") {
+        return "Until next time!"
     } else {
         return "That really depends"
     }
@@ -17,3 +21,57 @@ func responseTo(question: String) -> String {
 responseTo(question: "hello there!")
 responseTo(question: "where should I go on holiday?")
 responseTo(question: "what is the capital of France?")
+responseTo(question: "when should we meet?") 
+responseTo(question: "sorry, can you repeat that?") 
+responseTo(question: "goodbye then")
+
+// Change the case of the text before you try to match it, using the lowercased method:
+let question = "WHERE ARE THE COOKIES?"
+let lowerQuestion = question.lowercased()
+print(lowerQuestion.hasPrefix("where"))     // --> returns true
+                        
+// Exercise: Rewrite the function below to work with aNy cAsE of QueStiOn TeXT, so you get a correct answer
+func responseTo(question: String) -> String {
+    let lowerQ = question.lowercased()
+    if lowerQ.hasPrefix("hello") {
+        return "Why, hello there"
+    } else {
+        return "That really depends"
+    }
+}
+
+print(responseTo(question: "Hello!"))
+
+
+// Special Questions
+// Exercise: Change the function below so that you get the answer “In the cookie jar!” to 
+// the question “Where are the cookies?”
+func responseTo(question: String) -> String {
+    
+    let lowerQuestion = question.lowercased()
+    
+    if lowerQuestion == "where are the cookies?" {
+        return "In the cookie jar!"
+    } else if lowerQuestion.hasPrefix("where") {
+        return "To the North!"
+    } else {
+        return "That really depends"
+    }
+}
+
+print(responseTo(question: "Where are the cookies?"))
+
+
+// Default Answers
+// Return the length of a String
+print("hello".characters.count)
+
+// Use the remainder operator to get the remainder of the count after dividing by two:
+print("a".characters.count % 2)       // --> 1
+print("ab".characters.count % 2)      // --> 0
+print("abc".characters.count % 2)     // --> 1
+print("abcd".characters.count % 2)    // --> 0
+
+// You can convert any string into 0 or 1, then use the result to decide on an answer:
+// Exercise: Amend the function below to choose from one of three default answers instead of two.
+// Hint: question.characters.count % 3 will give you a result of 0, 1 or 2
